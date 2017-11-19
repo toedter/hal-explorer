8549d9d3f4af978d2674ca1a4c1b26eb789ca967
Kai Toedter <kai@toedter.com>
2017-11-19 08:58:30 +0100

    added baootswatch themes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,51 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  private themes: string[] = [
+    'Default',
+    'Cerulean',
+    'Cosmo',
+    'Cyborg',
+    'Darkly',
+    'Flatly',
+    'Journal',
+    'Litera',
+    'Lumen',
+    'Lux',
+    'Materia',
+    'Minty',
+    'Pulse',
+    'Sandstone',
+    'Simplex',
+    'Sketchy',
+    'Slate',
+    'Solar',
+    'Spacelab',
+    'Superhero',
+    'United',
+    'Yeti'
+  ];
+
+  private isCustomTheme = false;
+  private selectedThemeUrl: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) {
+  };
+
+  changeTheme(theme: string) {
+    if (theme === this.themes[0]) {
+      this.isCustomTheme = false;
+    } else {
+      this.isCustomTheme = true;
+      this.selectedThemeUrl =
+        this.sanitizer.bypassSecurityTrustResourceUrl('http://bootswatch.com/4/' + theme.toLowerCase() + '/bootstrap.min.css');
+    }
+  }
+}
