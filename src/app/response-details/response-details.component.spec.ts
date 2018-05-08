b3a6312ad42e78f41029c9b8d9f355b0038a07d5
Kai Toedter <kai@toedter.com>
2018-05-08 21:21:54 +0200

    test: prepared response details component spec

--- src/app/response-details/response-details.component.spec.ts
+++ src/app/response-details/response-details.component.spec.ts
@@ -1,10 +1,29 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+///<reference path="../../../node_modules/rxjs/internal/Observable.d.ts"/>
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ResponseDetailsComponent } from './response-details.component';
+import {ResponseDetailsComponent} from './response-details.component';
 import {RequestService} from '../request/request.service';
-import {HttpClient, HttpHandler} from '@angular/common/http';
-import {AppService} from '../app.service';
 import {JsonHighlighterService} from '../json-highlighter/json-highlighter.service';
+import {HttpResponse} from '@angular/common/http';
+
+class ObservableMock {
+  subscribe(next?: (value: HttpResponse<any>) => void, error?: (error: any) => void) {
+    // console.log('subscribed');
+    next(new HttpResponse<any>());
+  }
+}
+
+class RequestServiceMock {
+  getResponseObservable() {
+    return new ObservableMock();
+  }
+}
+
+class JsonHighlighterServiceMock {
+  syntaxHighlight() {
+    // console.log('syntaxHighlight invoked');
+  }
+}
 
 describe('ResponseDetailsComponent', () => {
   let component: ResponseDetailsComponent;
@@ -12,10 +31,12 @@ describe('ResponseDetailsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ResponseDetailsComponent ],
-      providers: [RequestService, AppService, JsonHighlighterService, HttpClient, HttpHandler]
-    })
-    .compileComponents();
+      declarations: [ResponseDetailsComponent],
+      providers: [
+        {provide: RequestService, useClass: RequestServiceMock},
+        {provide: JsonHighlighterService, useClass: JsonHighlighterServiceMock}
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
