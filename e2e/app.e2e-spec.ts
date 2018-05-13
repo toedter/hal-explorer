3a36636f4c60dd55c2b8f31feaa71a45638ffa4b
Kai Toedter <kai@toedter.com>
2018-05-13 08:21:59 +0200

    test: added more e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -5,6 +5,12 @@ import {AppConfig} from './app.config';
 describe('HAL-Explorer App', () => {
   let page: AppPage;
 
+  const expectResponseDetailsAreDisplayed = function () {
+    expect(page.getResponseStatusSection().isDisplayed()).toBeTruthy();
+    expect(page.getResponseHeadersSection().isDisplayed()).toBeTruthy();
+    expect(page.getResponseBodySection().isDisplayed()).toBeTruthy();
+  };
+
   beforeEach(() => {
     page = new AppPage();
   });
@@ -21,12 +27,30 @@ describe('HAL-Explorer App', () => {
 
   it('should not display HAL sections at startup', () => {
     page.navigateTo();
-    expect(page.getPropertiesSection().isPresent()).toBeFalsy();
+    expect(page.getFirstPropertiesSection().isPresent()).toBeFalsy();
+    expect(page.getFirstLinksSection().isPresent()).toBeFalsy();
+    expect(page.getEmbeddedSection().isPresent()).toBeFalsy();
+    expect(page.getResponseStatusSection().isDisplayed()).toBeFalsy();
+    expect(page.getResponseHeadersSection().isDisplayed()).toBeFalsy();
+    expect(page.getResponseBodySection().isDisplayed()).toBeFalsy();
+
   });
 
-  it('should display HAL sections when rendering HAL resource', () => {
+  it('should display HAL sections when rendering users resource', () => {
     page.navigateTo('/#url=' + AppConfig.getChattyApiUrl() + '/users');
-    expect(page.getPropertiesSection().isPresent()).toBeTruthy();
+    expect(page.getFirstPropertiesSection().isDisplayed()).toBeTruthy();
+    expect(page.getFirstLinksSection().isDisplayed()).toBeTruthy();
+    expect(page.getEmbeddedSection().isDisplayed()).toBeTruthy();
+    expectResponseDetailsAreDisplayed()
+  });
+
+  it('should display only Links section when rendering root api', () => {
+    page.navigateTo('/#url=' + AppConfig.getChattyApiUrl());
+    expect(page.getFirstPropertiesSection().isPresent()).toBeFalsy();
+    expect(page.getFirstLinksSection().isDisplayed()).toBeTruthy();
+    expect(page.getEmbeddedSection().isPresent()).toBeFalsy();
+    expectResponseDetailsAreDisplayed();
   });
 
 });
+
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -14,8 +14,38 @@ export class AppPage {
     return element(by.buttonText('Edit Headers')).getText();
   }
 
-  getPropertiesSection(): ElementFinder {
-    const elementFinder = element(by.id('propertiesSection'));
+  getFirstPropertiesSection(): ElementFinder {
+    const elementFinder = element.all(by.cssContainingText('h5', 'JSON Properties')).first();
+    return elementFinder;
+  }
+
+  getFirstLinksSection(): ElementFinder {
+    const elementFinder = element.all(by.cssContainingText('h5', 'Links')).first();
+    return elementFinder;
+  }
+
+  getEmbeddedSection(): ElementFinder {
+    const elementFinder = element(by.cssContainingText('h5', 'Embedded Resources'));
+    return elementFinder;
+  }
+
+  getResponseStatusSection(): ElementFinder {
+    const elementFinder = element(by.cssContainingText('h5', 'Response Status'));
+    return elementFinder;
+  }
+
+  getResponseHeadersSection(): ElementFinder {
+    const elementFinder = element(by.cssContainingText('h5', 'Response Headers'));
+    return elementFinder;
+  }
+
+  getResponseBodySection(): ElementFinder {
+    const elementFinder = element(by.cssContainingText('h5', 'Response Body'));
+    return elementFinder;
+  }
+
+  getDocumentationSection(): ElementFinder {
+    const elementFinder = element(by.cssContainingText('h5', 'Documentation'));
     return elementFinder;
   }
 }
