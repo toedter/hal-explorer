5c34b0f66fedcfd5963299eb8e9ebaececd9eb93
Kai Toedter <kai@toedter.com>
2018-05-14 22:15:39 +0200

    test: more unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,9 @@
-import {async, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, getTestBed, TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
 import {Component} from '@angular/core';
 import {AppService} from './app.service';
 import {RequestService} from './request/request.service';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-url-input',
@@ -33,12 +34,61 @@ class MockDocumentationComponent {
 }
 
 class AppServiceMock {
+  themeObservable: ObservableMock = new ObservableMock();
+  layoutObservable: ObservableMock = new ObservableMock();
+
+  getTheme(): string {
+    return 'Default';
+  }
+
+  setTheme(theme: string) {
+  }
+
+  getLayout(): string {
+    return '2 Columns';
+  }
+
+  setLayout(layout: string) {
+  }
+}
+
+class ObservableMock {
+  private callback: Function;
+  hasSubscribed = false;
+
+  subscribe(next?: (value: any) => void, error?: (error: any) => void) {
+    this.callback = next;
+    this.hasSubscribed = true;
+  }
+
+  next(input: any) {
+    this.callback(input);
+  }
 }
 
 class RequestServiceMock {
+  responseObservableMock: ObservableMock = new ObservableMock();
+  documentationObservableMock: ObservableMock = new ObservableMock();
+
+  public getResponseObservable() {
+    return this.responseObservableMock;
+  }
+
+  public getDocumentationObservable() {
+    return this.documentationObservableMock;
+  }
+}
+
+class DomSanitizerMock {
+  bypassSecurityTrustResourceUrl(docUri) {
+    return docUri;
+  }
 }
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -50,18 +100,46 @@ describe('AppComponent', () => {
       ],
       providers: [
         {provide: AppService, useClass: AppServiceMock},
-        {provide: RequestService, useClass: RequestServiceMock}
+        {provide: RequestService, useClass: RequestServiceMock},
+        {provide: DomSanitizer, useClass: DomSanitizerMock}
       ]
     }).compileComponents();
+
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   }));
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   }));
+
   it(`should not show documentation`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.showDocumentation).toEqual(false);
+    expect(component.showDocumentation).toBeFalsy();
   }));
+
+  it('should show documentation', () => {
+    const requestServiceMock: RequestServiceMock = getTestBed().get(RequestService);
+
+    requestServiceMock.documentationObservableMock.next('/doc');
+
+    expect(component.showDocumentation).toBeTruthy();
+  });
+
+  it('should not show documentation after getting response', () => {
+    const requestServiceMock: RequestServiceMock = getTestBed().get(RequestService);
+
+    requestServiceMock.documentationObservableMock.next('/doc');
+    requestServiceMock.responseObservableMock.next('response');
+
+    expect(component.showDocumentation).toBeFalsy();
+  });
+
+  it('should change theme', () => {
+    component.changeTheme('Cosmo');
+
+    expect(component.isCustomTheme).toBeTruthy();
+  });
+
 });
