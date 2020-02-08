de7ad670fff67778d04dd771b963b6280cbddd1b
Kai Toedter <kai@toedter.com>
2020-02-08 19:12:51 +0100

    feat: doc iframes are now expanded to full iframe height
    Thx to https://www.dyn-web.com/tutorials/iframes/height/

--- src/app/documentation/documentation.component.ts
+++ src/app/documentation/documentation.component.ts
@@ -26,3 +26,23 @@ export class DocumentationComponent implements OnInit {
       });
   }
 }
+
+// The following functions are from https://www.dyn-web.com/tutorials/iframes/height/
+export function getDocHeight(doc) {
+  doc = doc || document;
+  // stackoverflow.com/questions/1145850/
+  const body = doc.body, html = doc.documentElement;
+  const height = Math.max(body.scrollHeight, body.offsetHeight,
+    html.clientHeight, html.scrollHeight, html.offsetHeight);
+  return height;
+}
+
+(<any>window).setIframeHeight = function(id) {
+  const iFrame: any = document.getElementById(id);
+  const doc = iFrame.contentDocument ? iFrame.contentDocument :
+    iFrame.contentWindow.document;
+  iFrame.style.visibility = 'hidden';
+  iFrame.style.height = '10px'; // reset to minimal height ...
+  iFrame.style.height = getDocHeight(doc) + 4 + 'px';
+  iFrame.style.visibility = 'visible';
+}
