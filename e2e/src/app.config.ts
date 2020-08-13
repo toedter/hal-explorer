5947d846a3d474c3c722396a7ef0f5259c96cd80
Kai Toedter <kai@toedter.com>
2020-08-13 10:28:16 +0200

    chore: add comment to e2e config

--- e2e/src/app.config.ts
+++ e2e/src/app.config.ts
@@ -5,8 +5,8 @@ export class AppConfig {
     const operatingSystem = os.platform();
     if (operatingSystem === 'win32') {
       // this is the default docker ip for running Docker within VirtualBox
-      // TODO: find out, if Hyper-V is used
-      return 'http://192.168.99.100:8080/api';
+      // if Docker for Windows is used, replace 192.168.99.100 with 127.0.0.1
+      return 'http://192.168.99.101:8080/api';
     }
     return 'http://127.0.0.1:8080/api';
   }
