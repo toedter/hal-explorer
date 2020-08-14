bcfe57b9a8e651a9ba5d3b5e29d2861a1c64f89e
Kai Toedter <kai@toedter.com>
2020-08-14 13:35:37 +0200

    chore: improve default Windows Docker config

--- e2e/src/app.config.ts
+++ e2e/src/app.config.ts
@@ -1,13 +1,7 @@
-import * as os from 'os';
-
 export class AppConfig {
   static getChattyApiUrl()  {
-    const operatingSystem = os.platform();
-    if (operatingSystem === 'win32') {
-      // this is the default docker ip for running Docker within VirtualBox
-      // if Docker for Windows is used, replace 192.168.99.100 with 127.0.0.1
-      return 'http://192.168.99.101:8080/api';
-    }
-    return 'http://127.0.0.1:8080/api';
+    // if you are using Docker with docker-machine and VirtualBox on Windows,
+    // replace localhost with your docker-machine's ip, e.g. 192.168.99.100
+    return 'http://localhost:8080/api';
   }
 }
