915897880abd39e2aafbfe93428395df631174ca
Kai Toedter <kai@toedter.com>
2021-01-24 17:46:20 +0100

    chore: use Jasmine spies in response-details component spec

--- src/app/response-details/response-details.component.spec.ts
+++ src/app/response-details/response-details.component.spec.ts
@@ -1,53 +1,28 @@
-import { ComponentFixture, getTestBed, TestBed, waitForAsync } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
-import { ResponseDetailsComponent } from './response-details.component';
-import { Command, RequestService } from '../request/request.service';
-import { JsonHighlighterService } from '../json-highlighter/json-highlighter.service';
-import { HttpHeaders, HttpResponse } from '@angular/common/http';
-
-class ObservableMock {
-  private callback: (value: HttpResponse<any>) => void;
-  hasSubscribed = false;
-
-  subscribe(next?: (value: HttpResponse<any>) => void, error?: (error: any) => void) {
-    this.callback = next;
-    this.hasSubscribed = true;
-  }
-
-  next(response: HttpResponse<any>) {
-    this.callback( response );
-  }
-}
-
-class RequestServiceMock {
-  observableMock: ObservableMock = new ObservableMock();
-  requestServiceProcessCommandInvoked = false;
-
-  getResponseObservable() {
-    return this.observableMock;
-  }
-
-  processCommand(command: Command, link: string) {
-    this.requestServiceProcessCommandInvoked = true;
-  }
-}
-
-class JsonHighlighterServiceMock {
-  syntaxHighlight() {
-    // console.log('syntaxHighlight invoked');
-  }
-}
+import {ResponseDetailsComponent} from './response-details.component';
+import {RequestService} from '../request/request.service';
+import {JsonHighlighterService} from '../json-highlighter/json-highlighter.service';
+import {HttpHeaders, HttpResponse} from '@angular/common/http';
+import {Subject} from 'rxjs';
 
 describe( 'ResponseDetailsComponent', () => {
   let component: ResponseDetailsComponent;
   let fixture: ComponentFixture<ResponseDetailsComponent>;
+  let responseSubject;
 
   beforeEach( waitForAsync( () => {
+    const requestServiceMock = jasmine.createSpyObj(['getResponseObservable']);
+    responseSubject = new Subject<string>();
+    requestServiceMock.getResponseObservable.and.returnValue(responseSubject);
+
+    const jsonHighlighterServiceMock = jasmine.createSpyObj(['syntaxHighlight']);
+
     TestBed.configureTestingModule( {
       declarations: [ResponseDetailsComponent],
       providers: [
-        { provide: RequestService, useClass: RequestServiceMock },
-        { provide: JsonHighlighterService, useClass: JsonHighlighterServiceMock }
+        { provide: RequestService, useValue: requestServiceMock },
+        { provide: JsonHighlighterService, useValue: jsonHighlighterServiceMock }
       ]
     } ).compileComponents();
   } ) );
@@ -63,38 +38,32 @@ describe( 'ResponseDetailsComponent', () => {
   } );
 
   it( 'should handle json HTTP response', () => {
-    const requestServiceMock: RequestServiceMock = getTestBed().inject( RequestService ) as any;
-
-    requestServiceMock.getResponseObservable().next( new HttpResponse( { body: { key: 'test' } } ) );
+    responseSubject.next( new HttpResponse( { body: { key: 'test' } } ) );
 
     expect( component.responseStatus ).toBe( 200 );
     expect( component.isString ).toBeFalsy();
   } );
 
   it( 'should handle string HTTP response', () => {
-    const requestServiceMock: RequestServiceMock = getTestBed().inject( RequestService ) as any;
-
-    requestServiceMock.getResponseObservable().next( new HttpResponse( { body: 'string' } ) );
+    responseSubject.next( new HttpResponse( { body: 'string' } ) );
 
     expect( component.responseStatus ).toBe( 200 );
     expect( component.isString ).toBeTruthy();
   } );
 
   it( 'should handle HTTP response headers', () => {
-    const requestServiceMock: RequestServiceMock = getTestBed().inject( RequestService ) as any;
     const responseHeaders: HttpHeaders = new HttpHeaders(
       {
         key: 'value'
       } );
-    requestServiceMock.getResponseObservable().next( new HttpResponse( { headers: responseHeaders } ) );
+    responseSubject.next( new HttpResponse( { headers: responseHeaders } ) );
 
     expect( component.responseStatus ).toBe( 200 );
     expect( component.responseHeaders.length ).toBe( 1 );
   } );
 
   it( 'should handle HTTP response status 0', () => {
-    const requestServiceMock: RequestServiceMock = getTestBed().inject( RequestService ) as any;
-    requestServiceMock.getResponseObservable().next( new HttpResponse( { status: 0 } ) );
+    responseSubject.next( new HttpResponse( { status: 0 } ) );
 
     expect( component.responseStatus ).toBe( 0 );
     expect( component.responseStatusText ).toBe( '' );
