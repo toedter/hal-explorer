238572bce5e573c528ea2b497c9f20749413a8dc
Kai Toedter <kai@toedter.com>
2021-04-11 13:33:59 +0200

    fix: fix http status text in response details
    - e.g. Angular returns: 201, OK
    - Now: 201, Created

--- src/app/response-details/response-details.component.ts
+++ src/app/response-details/response-details.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {RequestService, Response} from '../request/request.service';
 import {HttpErrorResponse, HttpResponse} from '@angular/common/http';
 import {JsonHighlighterService} from '../json-highlighter/json-highlighter.service';
+import {getReasonPhrase} from 'http-status-codes';
 
 @Component({
   selector: 'app-response-details',
@@ -17,6 +18,8 @@ export class ResponseDetailsComponent implements OnInit {
   httpErrorResponse: HttpErrorResponse;
   httpResponse: HttpResponse<any>;
 
+  httpResponseReasonPhrase: string;
+
   constructor(private requestService: RequestService, private jsonHighlighterService: JsonHighlighterService) {
   }
 
@@ -25,7 +28,9 @@ export class ResponseDetailsComponent implements OnInit {
       .subscribe((response: Response) => {
           this.httpResponse = response.httpResponse;
           this.httpErrorResponse = response.httpErrorResponse;
+          this.httpResponseReasonPhrase = 'Unknown';
           if (this.httpResponse) {
+            this.httpResponseReasonPhrase = getReasonPhrase(this.httpResponse.status);
             this.responseBody = undefined;
             if (this.httpResponse.body) {
               if (typeof this.httpResponse.body === 'string' || this.httpResponse.body instanceof String) {
@@ -39,6 +44,10 @@ export class ResponseDetailsComponent implements OnInit {
             }
           } else if (this.httpErrorResponse) {
             this.error = undefined;
+            if (this.httpErrorResponse.status > 200) {
+              this.httpResponseReasonPhrase = getReasonPhrase(this.httpErrorResponse.status);
+            }
+
             if (this.httpErrorResponse.error) {
               if (typeof this.httpErrorResponse.error === 'string' || this.httpErrorResponse.error instanceof String) {
                 this.isString = true;
