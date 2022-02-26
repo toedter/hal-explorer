5cdd49f8a146f6806f567d59aa49c13d696b3cbd
Kai Toedter <kai@toedter.com>
2022-02-26 11:11:56 +0100

    chore: prepare version 1.1.1-SNAPSHOT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -57,7 +57,7 @@ export class AppComponent implements OnInit {
   isTwoColumnLayout = true;
   useHttpOptions = false;
 
-  version = '1.1.0';
+  version = '1.1.1-SNAPSHOT';
   isSnapshotVersion = this.version.endsWith('SNAPSHOT');
 
   constructor(
