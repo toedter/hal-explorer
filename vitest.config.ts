c5e414810524ed73772b36b0968381090a476598
Kai Toedter <kai@toedter.com>
2025-12-24 09:46:01 +0100

    chore: add codecov.yml to exclude CSS and HTML from codecov coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,9 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import angular from '@analogjs/vite-plugin-angular';
 
 export default defineConfig({
-  plugins: [angular()],
+  plugins: [angular() as any],
   test: {
     globals: true,
     environment: 'jsdom',
@@ -15,6 +15,19 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['text', 'html', 'lcov'],
       reportsDirectory: './coverage/vitest',
+      exclude: [
+        '**/*.html',
+        '**/*.css',
+        'src/test-setup.ts',
+        'src/test.ts',
+        'src/polyfills.ts',
+        'src/main.ts',
+        'src/environments/**',
+        '**/*.spec.ts',
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/coverage/**',
+      ],
     },
   },
 });
